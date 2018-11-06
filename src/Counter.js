1f1e66403a8470c7fef551355beacf06d2d5ce8f
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-06 22:58:05 +0900

    online event

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
-export default () => {
-  const [count, setCount] = useState(0)
+function useOnline() {
+  const [online, setOnline] = useState(navigator.onLine)
+
+  function onOnline() {
+    setOnline(true)
+  }
+
+  function onOffline() {
+    setOnline(false)
+  }
 
   useEffect(() => {
-    console.info('re rendered')
+    window.addEventListener('online', onOnline)
+
+    window.addEventListener('offline', onOffline)
+
+    return () => { 
+      window.removeEventListener('online', onOnline)
+      window.removeEventListener('offline', onOffline)
+     }
   })
+
+  return [online]
+}
+
+export default () => {
+  const [count, setCount] = useState(0)
+  const [online] = useOnline()
 
   return <div>
     <p>{count}</p>
     <button onClick={() => setCount(count + 1)}>Incrment</button>
+    <p>Network is now {online ? 'ONLINE' : 'OFFLINE'}</p>
   </div>
 };
