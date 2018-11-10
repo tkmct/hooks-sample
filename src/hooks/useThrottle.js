da88f0a88188f94da0b92d7358a35892f6a7bfa2
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-11 00:48:38 +0900

    use throttle initial

--- src/hooks/useThrottle.js
+++ src/hooks/useThrottle.js
@@ -1,4 +1,9 @@
 import { useState, useEffect } from 'react'
 
+// Throttle hook.
+// newValue is returned after given duration of time.
 function useThrottle(value, duration) {
+  const [newValue, setNewValue] = useState(value)
+
+  return newValue
 }
