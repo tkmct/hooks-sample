72e46022d969d55238b37fe53fd376e756b99d71
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-11 00:45:09 +0900

    clean up

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
-import React, { Component, Suspense, lazy, useState } from 'react'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { NetworkProvider } from './contexts/NetworkContext'
-import useDebounce from './hooks/useDebounce'
-
-const CounterLazy = lazy(() => import('./components/Counter'))
-const FormLazy = lazy(() => import('./components/Form'))
-const NetworkStatusBarLazy = lazy(() => import('./components/NetworkStatusBar'))
-
-const Scroller = () => {
-  const [scrollY, setScrollY] = useState(0)
-  const debouncedScrollY = useDebounce(scrollY, 500)
-
-  return <React.Fragment>
-    <span style={{ display: 'block'}}>
-      scroll: {scrollY},
-      debounced: {debouncedScrollY}
-    </span>
-    <div style={{ position: 'relative', overflow: 'scroll', height: 200, border: 'solid 1px gray' }} onScroll={() => setScrollY(Math.random())}>
-      <div style={{ minHeight: 1400 }}>
-      </div>
-    </div>
-  </React.Fragment>
-}
+import Counter from './components/Counter'
+import Form from './components/Form'
+import NetworkStatusBar from './components/NetworkStatusBar'
+import Scroller from './components/Scroller'
 
 export default class App extends Component {
   render() {
-    return <NetworkProvider>
-      <h1>Counter</h1>
-      <Suspense fallback={<p>Loading Counter...</p>}>
-        <CounterLazy />
-      </Suspense>
-      <Suspense fallback={<p>Loading Counter...</p>}>
-        <FormLazy />
-      </Suspense>
-      <Suspense fallback={""}>
-        <NetworkStatusBarLazy />
-      </Suspense>
-      <Scroller />
-    </NetworkProvider>
+    return <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Counter</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+            <li>
+              <Link to="/network">Network</Link>
+            </li>
+            <li>
+              <Link to="/scroller">Scroller</Link>
+            </li>
+          </ul>
+        </nav>
+        <NetworkProvider>
+          <Route exact path="/" component={Counter} />
+          <Route exact path="/form" component={Form} />
+          <Route exact path="/network" component={NetworkStatusBar} />
+          <Route exact path="/scroller" component={Scroller} />
+        </NetworkProvider>
+      </div>
+      </Router>
   }
 }
---
+++ src/components/Scroller.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import useDebounce from '../hooks/useDebounce'
+
+
+const Scroller = () => {
+  const [scrollY, setScrollY] = useState(0)
+  const debouncedScrollY = useDebounce(scrollY, 500)
+
+  return <React.Fragment>
+    <span style={{ display: 'block'}}>
+      scroll: {scrollY},
+      debounced: {debouncedScrollY}
+    </span>
+    <div style={{ position: 'relative', overflow: 'scroll', height: 200, border: 'solid 1px gray' }} onScroll={() => setScrollY(Math.random())}>
+      <div style={{ minHeight: 1400 }}>
+      </div>
+    </div>
+  </React.Fragment>
+}
+
+export default Scroller
